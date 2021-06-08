d9062b5c33162449d01760c1b036dd435fbd59f7
Marbett Hedderich <marbett@gmail.com>
2021-06-09 00:45:26 +0200

    listado heroes y detalle heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  heroe!: Heroe;
+  constructor(private activatedRoute: ActivatedRoute, 
+              private heroeService: HeroesService,
+              private router: Router) { 
+              
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap (({id}) => this.heroeService.getHeroeById(id))
+    )
+      .subscribe (heroe => this.heroe = heroe);
+  }
+  regresar () {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesService.getHeroes()
+          .subscribe( heroes => {
+            console.log(heroes);
+            this.heroes = heroes;
+
+          });
   }
 
 }
